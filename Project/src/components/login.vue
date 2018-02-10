3f057383398ab751e989663bf705e9628b92ffee
ConnorChristensen <chriconn@oregonstate.edu>
2018-02-09 17:02:11 -0800

    got reactive routing working When setting the username to admin, the page responds by changing the submit button route to the admin page. Some errors were corrected that was causing the vue.js code to not be loading properly. This was fixed as well as code being beautified.

--- Project/src/components/login.vue
+++ Project/src/components/login.vue
@@ -8,11 +8,10 @@
       <img src="../assets/logo.png" class="logo-med">
       <h2>Login</h2>
       <input v-model.lazy="username" placeholder="username">
-      <p>{{ feedback.username }}</p>
-      <input v-model.lazy="password" placeholder="passwword">
-      <p>{{ feedback.password }}</p>
-      <router-link to="/home"><button>Submit</button> </router-link>
-      <router-link to="/admin">Go to admin page</router-link>
+      <span>{{ feedback.username }}</span>
+      <input v-model.lazy="password" placeholder="password">
+      <span>{{ feedback.password }}</span>
+      <router-link v-bind:to="submitLink"><button>Submit</button> </router-link>
     </div>
   </div>
 </div>
@@ -31,51 +30,57 @@ default {
         username: '',
         password: '',
       },
+      submitLink: '',
     };
   },
-  watch() {
-    return {
-      username() {
-        // shorten our username variable for readability
-        const username = this.username;
+  watch: {
+    username: function() {
+      // shorten our username variable for readability
+      const username = this.username;
 
-        // if the field is empty, clear the feedback
-        // if the username is less than 3 or greater than 20, error
-        // if the username is only numbers and letters
-        // else there are illegal characters
-        if (username.length === 0) {
-          this.feedback.username = ''
-        } else if (username.length < 3 || username.length > 20) {
-          this.feedback.username = 'username must be between 3 and 20 characters'
-        } else if (username.match('^[0-9A-z]+$')) {
-          this.feedback.username = ''
-        } else {
-          this.feedback.username = 'username must only be digits and letters'
-        }
-      },
-      password() {
-        // shorten our username variable for readability
-        const password = this.password
+      if (username === 'admin') {
+        this.submitLink = '/admin'
+      }
 
-        // if the field is empty, clear the feedback
-        // if the password is less than 6 or greater than 25, error
-        // if the password is numbers, letters and some select special characters
-        // else there are illegal characters
-        if (password.length === 0) {
-          this.feedback.password = ''
-        } else if (password.length < 6 || password.length > 25) {
-          this.feedback.password = 'password must be between 6 and 25 characters'
-        } else if (password.match('^[0-9A-z@#$%*^!\-=&]+$')) {
-          this.feedback.password = ''
-        } else {
-          this.feedback.password = 'password must only be digits and letters'
-        }
-      },
-    };
+      // if the field is empty, clear the feedback
+      // if the username is less than 3 or greater than 20, error
+      // if the username is only numbers and letters
+      // else there are illegal characters
+      if (username.length === 0) {
+        this.feedback.username = ''
+      } else if (username.length < 3 || username.length > 20) {
+        this.feedback.username = 'username must be between 3 and 20 characters'
+      } else if (username.match('^[0-9A-z]+$')) {
+        this.feedback.username = ''
+      } else {
+        this.feedback.username = 'username must only be digits and letters'
+      }
+    },
+    password: function() {
+      // shorten our username variable for readability
+      const password = this.password
+
+      // if the field is empty, clear the feedback
+      // if the password is less than 6 or greater than 25, error
+      // if the password is numbers, letters and some select special characters
+      // else there are illegal characters
+      if (password.length === 0) {
+        this.feedback.password = ''
+      } else if (password.length < 6 || password.length > 25) {
+        this.feedback.password = 'password must be between 6 and 25 characters'
+      } else if (password.match('^[0-9A-z@#$%*^!\-=&]+$')) {
+        this.feedback.password = ''
+      } else {
+        this.feedback.password = 'password must only be digits and letters'
+      }
+    }
   },
   beforeMount() {
     if (/iPhone|iPad|iPod|Android|webOS|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-      mobile = true
+      this.mobile = true
+      this.submitLink = '/tank-info'
+    } else {
+      this.submitLink = '/home'
     }
   }
 };
