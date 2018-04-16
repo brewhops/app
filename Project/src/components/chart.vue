237d9f52e4a7c729b1b881ee6e9f32b08782232a
ConnorChristensen <chriconn@oregonstate.edu>
2018-04-15 19:22:28 -0700

    Charts update on data change The asynchronous nature of the database requests made the charts load before the data was retreived. The charts will now update whenever the y axis data is modified.

--- Project/src/components/chart.vue
+++ Project/src/components/chart.vue
@@ -8,33 +8,31 @@ const c3 = require('c3')
 // if you dont specify a starting location, webpack will look in the node_modules folder
 import 'c3/c3.min.css'
 
-export
-default {
+export default {
   name: 'chart',
   props: ['date', 'data'],
   data() {
     return {};
   },
-  mounted: function() {
-    let data = this.data
-    let date = this.date
-    console.log(data);
-    c3.generate({
-      bindto: this.$el,
-      data: {
-        x: 'Date', //bind the x axis to the 'Date' data set
-        xFormat: '%m/%d/%Y %H:%M',
-        columns: [ date, data ]
-      },
-      axis: {
-        x: {
-          type: 'timeseries', // the x axis has a timeseries data type
-          tick: {
-            format: '%m-%d %H:%M'
+  watch: {
+    data: function() {
+      c3.generate({
+        bindto: this.$el,
+        data: {
+          x: 'Date', //bind the x axis to the 'Date' data set
+          xFormat: '%m/%d/%Y %H:%M',
+          columns: [ this.date, this.data ]
+        },
+        axis: {
+          x: {
+            type: 'timeseries', // the x axis has a timeseries data type
+            tick: {
+              format: '%m-%d %H:%M'
+            }
           }
-        }
-      }
-    });
+        },
+      });
+    }
   }
 };
 </script>
