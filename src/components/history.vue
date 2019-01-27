d154426f79544d8dc6f40b6a110be2402f5ad7ac
danvanhorn <dan.vanhorn17@gmail.com>
2019-01-26 23:04:00 -0800

    feat(history): get data for batch history page

--- src/components/history.vue
+++ src/components/history.vue
@@ -9,7 +9,9 @@
         <h2>Batch</h2>
         <select v-model="batch_id" v-on:change="batchChoose">
           <option disabled value="">Batch</option>
-          <option v-for="batch in batches" v-bind:value="batch.id">{{ batch.batch_name }}</option>
+          <option v-for="(batch, idx) in batches" v-bind:key="idx" v-bind:value="batch.id">{{
+            batch.name
+          }}</option>
         </select>
       </div>
       <table v-if="batch_id && histories && batch">
@@ -22,11 +24,11 @@
           <th>pressure</th>
         </tr>
         <tr v-for="history in histories">
-          <td>{{ history.updated_at }}</td>
-          <td>{{ history.SG }}</td>
-          <td>{{ history.pH }}</td>
-          <td>{{ history.ABV }}</td>
-          <td>{{ history.temp }}</td>
+          <td>{{ history.measured_on }}</td>
+          <td>{{ history.sg }}</td>
+          <td>{{ history.ph }}</td>
+          <td>{{ history.abv }}</td>
+          <td>{{ history.temperature }}</td>
           <td>{{ history.pressure }}</td>
         </tr>
         <tr>
@@ -59,15 +61,16 @@ import Vue from 'vue';
 import router from '../router/index.js';
 import Cookie from 'js-cookie';
 import moment from 'moment';
+import { Batch } from '../types';
 
 // tslint:disable: no-any
 
 interface IHistoryState {
-  mobile: any;
-  batch_id: any;
-  batches: any;
-  batch: any;
-  histories: any;
+  mobile: boolean;
+  batch_id: number | string;
+  batches: Batch[];
+  batch: Batch;
+  histories: any[];
 }
 
 export default Vue.extend({
@@ -77,22 +80,33 @@ export default Vue.extend({
       mobile: false,
       batch_id: '',
       batches: [],
-      batch: {},
+      batch: {
+        name: '',
+        generation: 0,
+        volume: 0,
+        bright: 0,
+        recipe_id: 0,
+        tank_id: 0,
+        started_on: '',
+        completed_on: ''
+      },
       histories: []
     };
   },
-  beforeMount() {
+  async beforeMount() {
     // if the user is not logged in send them to the login page
     if (!Cookie.getJSON('loggedIn')) {
       router.push('/');
     }
 
-    this.$http.get(`${process.env.API}/batches/`).then(batchesResponse => {
-      // get our batches data
-      this.batches = batchesResponse.body;
-      // sort the batches by ID
-      this.batches.sort((a, b) => a.id - b.id);
-    });
+    try {
+      const response = await this.$http.get(`${process.env.API}/batches/`);
+      this.batches = <Batch[]>response.data;
+      this.batches.sort((a: any, b: any) => a.id - b.id);
+    } catch (err) {
+      // tslint:disable-next-line:no-console
+      console.error(err);
+    }
   },
   methods: {
     home() {
@@ -101,96 +115,100 @@ export default Vue.extend({
       }
       router.push('/');
     },
-    batchChoose() {
+    async batchChoose() {
       // filter out all the batches that aren't ours, and set that one element
       // to our batch object
       this.batch = this.batches.filter(e => e.id === this.batch_id)[0];
 
       // when the user chooses a batch, get the info on that batch
-      this.$http
-        .get(`${process.env.API}/batches/${this.batch_id}/batch_contents_versions`)
-        .then(historyResponse => {
-          this.histories = historyResponse.body;
-          // for every batch history, format the date so its easy to read
-          for (let history of this.histories) {
-            history.updated_at = moment(history.updated_at).format('MM-DD-YY HH:mm');
-          }
-          // create our header
-          let rows = [
-            [
-              'Date',
-              'SG',
-              'pH',
-              'ABV',
-              'temp',
-              'pressure',
-              'Volume',
-              'Bright',
-              'Generation',
-              'Date Started',
-              'Date Completed'
-            ]
-          ];
+      try {
+        const batchResponse = await this.$http.get(
+          `${process.env.API}/versions/batch/${this.batch_id}`
+        );
 
-          // for each history element
-          for (let history of this.histories) {
-            // add in that history row
-            rows.push([
-              history.updated_at,
-              history.SG,
-              history.pH,
-              history.ABV,
-              history.temp,
-              history.pressure,
-              this.batch.volume,
-              this.batch.bright,
-              this.batch.generation,
-              this.batch.date_started,
-              this.batch.date_completed
-            ]);
-          }
+        this.histories = batchResponse.data;
+      } catch (err) {
+        // tslint:disable-next-line:no-console
+        console.error(err);
+      }
 
-          // create the header for the csv that we will download
-          let csvContent = 'data:text/csv;charset=utf-8,';
+      // create our header
+      const rows: string[][] = [
+        [
+          'Date',
+          'SG',
+          'pH',
+          'ABV',
+          'temp',
+          'pressure',
+          'Volume',
+          'Bright',
+          'Generation',
+          'Date Started',
+          'Date Completed'
+        ]
+      ];
 
-          // for every row, add a comma to the end and some new line chars
-          for (let row of rows) {
-            csvContent += row.join(',') + '\r\n';
-          }
+      // for each history element
+      for (const history of this.histories) {
+        // add in that history row
+        rows.push([
+          history.measured_on,
+          history.sg,
+          history.ph,
+          history.abv,
+          history.temperature,
+          history.pressure,
+          this.batch.volume,
+          this.batch.bright,
+          this.batch.generation,
+          this.batch.started_on,
+          this.batch.completed_on
+        ]);
+      }
 
-          // find the csvDownload link and set some info about what it links to
-          // and what the download file should be called
-          let link = document.getElementById('csvDownload');
-          if (link != null) {
-            link.setAttribute('href', encodeURI(csvContent));
-            link.setAttribute(
-              'download',
-              'batch_history_' +
-                this.batch.batch_name +
-                '_(' +
-                moment().format('MM-DD-YYYY') +
-                ').csv'
-            );
-          }
-        });
+      // create the header for the csv that we will download
+      let csvContent = 'data:text/csv;charset=utf-8,';
+
+      // for every row, add a comma to the end and some new line chars
+      for (const row of rows) {
+        csvContent = `${csvContent},${'\r\n'}`;
+      }
+
+      // find the csvDownload link and set some info about what it links to
+      // and what the download file should be called
+      const link = document.getElementById('csvDownload');
+      if (link) {
+        link.setAttribute('href', encodeURI(csvContent));
+        link.setAttribute(
+          'download',
+          `batch_history_${this.batch.name}_(${moment().format('MM-DD-YYYY')}).csv`
+        );
+      }
     }
   }
 });
 </script>
 
 <style lang="stylus" scoped>
+#content {
+  display: grid;
+  justify-items: center;
+  text-align: center;
+  min-height: 60vh;
 
-#content
-  display grid
-  justify-items center
-  text-align center
-  min-height 60vh
-  p
-    color Teal
-    font-weight bold
-  table
-    tr
-      td, th
-        padding 10px
-        border-left 1px solid black
+  p {
+    color: Teal;
+    font-weight: bold;
+  }
+
+  table {
+    tr {
+      td, th {
+        padding: 10px;
+        border-left: 1px solid black;
+      }
+    }
+  }
+}
 </style>
