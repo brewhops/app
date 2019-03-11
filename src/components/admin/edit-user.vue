cd50652d7e9ee5f36a2bbe6bd6ffca2e1bdccea1
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-03-11 15:55:38 -0700

    feat(edit-user): update post request to patch

--- src/components/admin/edit-user.vue
+++ src/components/admin/edit-user.vue
@@ -1,15 +1,11 @@
 <template>
   <div v-if="!employee" class="element">
     <h2>Edit User</h2>
-    <select v-model="employee">
+    <select id="edit-user" v-on:change="populateEmployee" v-model="employee">
       <option disabled value="">Select Employee</option>
-      <option
-        v-for="emp in employees"
-        v-on:click="populateEmployee"
-        v-bind:key="emp.id"
-        :value="emp"
-        >{{ `${emp.first_name} ${emp.last_name}` }}</option
-      >
+      <option v-for="emp in employees" v-bind:key="emp.id" :value="emp">{{
+        `${emp.first_name} ${emp.last_name}`
+      }}</option>
     </select>
   </div>
   <div v-else class="element">
@@ -24,16 +20,15 @@
     <span>{{ feedback.password }}</span>
     <input v-model.lazy="passwordcheck" placeholder="Re-enter password" type="password" />
     <span>{{ feedback.passwordcheck }}</span>
-
     <div class="checkbox">
       <input type="checkbox" id="checkbox" v-model="admin" />
       <p>User is admin</p>
     </div>
-
     <div>
       <p>{{ feedback.server.user }}</p>
     </div>
-    <button v-on:click="login_submit">Submit</button>
+    <button v-on:click="editSubmit">Submit</button>
+    <button v-on:click="clearUser">Select a User</button>
   </div>
 </template>
 
@@ -100,70 +95,59 @@ export default Vue.extend({
   watch: {
     first_name() {
       // shorten our username variable for readability
-      if (this.employee) {
-        const { first_name } = this.employee;
+      const { first_name } = this;
 
-        if (!first_name.match('^[A-z\-]+$')) {
-          this.feedback.first_name = 'First name can only be digits and letters';
-        }
+      if (!first_name.match('^[A-z\-]+$')) {
+        this.feedback.first_name = 'First name can only be digits and letters';
       }
     },
     last_name() {
       // shorten our username variable for readability
-      if (this.employee) {
-        const { last_name } = this.employee;
 
-        if (!last_name.match(/^[a-zA-Z\-\s]*$/)) {
-          this.feedback.last_name = 'Last name can only be letters, spaces, and hyphens';
-        }
+      const { last_name } = this;
+
+      if (!last_name.match(/^[a-zA-Z\-\s]*$/)) {
+        this.feedback.last_name = 'Last name can only be letters, spaces, and hyphens';
       }
     },
     username() {
-      if (this.employee) {
-        const { username } = this.employee;
+      const { username } = this;
 
-        // if the username is already in the  database,
-        // give feedback and exit the function
-        for (const x in this.employees) {
-          if (this.employees[x].username === username) {
-            this.feedback.username = 'Username taken';
+      // if the username is already in the  database,
+      // give feedback and exit the function
+      for (const x in this.employees) {
+        if (this.employees[x].username === username) {
+          this.feedback.username = 'Username taken';
 
-            return;
-          }
+          return;
         }
+      }
 
-        if (username.length === 0) {
-          this.feedback.username = 'Username required';
-        } else if (username.length < 3 || username.length > 20) {
-          this.feedback.username = 'Username must be between 3 and 20 characters';
-        } else if (username.match('^[0-9A-z]+$')) {
-          this.feedback.username = '';
-        } else {
-          this.feedback.username = 'Username must only be digits and letters';
-        }
+      if (username.length === 0) {
+        this.feedback.username = 'Username required';
+      } else if (username.length < 3 || username.length > 20) {
+        this.feedback.username = 'Username must be between 3 and 20 characters';
+      } else if (username.match('^[0-9A-z]+$')) {
+        this.feedback.username = '';
+      } else {
+        this.feedback.username = 'Username must only be digits and letters';
       }
     },
     password() {
-      if (this.employee && this.employee.password) {
-        const { password } = this.employee;
+      const { password } = this;
 
-        if (password.length === 0) {
-          this.feedback.password = 'Password required';
-        } else if (password.length < 6 || password.length > 25) {
-          this.feedback.password = 'Password must be between 6 and 25 characters';
-        } else if (password.match('^[0-9A-z@#$%*^!\-=&]+$')) {
-          this.feedback.password = '';
-        } else {
-          this.feedback.password = 'Password must only be digits and letters';
-        }
+      if (password.length === 0) {
+        this.feedback.password = 'Password required';
+      } else if (password.length < 6 || password.length > 25) {
+        this.feedback.password = 'Password must be between 6 and 25 characters';
+      } else if (password.match('^[0-9A-z@#$%*^!\-=&]+$')) {
+        this.feedback.password = '';
+      } else {
+        this.feedback.password = 'Password must only be digits and letters';
       }
     },
     passwordcheck() {
-      if (
-        this.employee &&
-        this.employee.password &&
-        this.passwordcheck === this.employee.password
-      ) {
+      if (this.password && this.passwordcheck === this.password) {
         this.feedback.passwordcheck = '';
       } else {
         this.feedback.passwordcheck = 'Passwords must match';
@@ -171,39 +155,56 @@ export default Vue.extend({
     }
   },
   methods: {
-    async login_submit() {
+    async editSubmit() {
       if (this.employee) {
-        this.employee.password = CryptoJS.SHA3(<string>this.employee.password).toString();
-      }
+        const user = {
+          first_name: this.first_name,
+          last_name: this.last_name,
+          username: this.username,
+          admin: this.admin,
+          phone: '',
+          password: CryptoJS.SHA3(this.password).toString()
+        };
 
-      const headers = {
-        Authorization: `Bearer ${Cookie.getJSON('loggedIn').token}`
-      };
+        const headers = {
+          Authorization: `Bearer ${Cookie.getJSON('loggedIn').token}`
+        };
 
-      try {
-        const response = await this.$http.post(`${process.env.API}/employees`, this.employee, {
-          headers
-        });
-        if (response.ok) {
-          this.feedback.server.user = 'New user succesfully created';
-          setTimeout(async () => (this.feedback.server.user = ``), 5000);
+        try {
+          const response = await this.$http.patch(
+            `${process.env.API}/employees/id/${this.employee.id}`,
+            user,
+            {
+              headers
+            }
+          );
+          if (response.ok) {
+            this.feedback.server.user = 'User succesfully edited.';
+            setTimeout(async () => (this.feedback.server.user = ``), 5000);
+          }
+        } catch (err) {
+          console.error(err);
+          this.feedback.server.user = 'Failed to edit user.';
         }
-      } catch (err) {
-        console.error(err);
-        this.feedback.server.user = 'Failed to create new user.';
       }
     },
     populateEmployee() {
       if (this.employee) {
-        const { first_name, last_name, password, admin, phone } = this.employee;
+        const { first_name, last_name, password, admin, username, phone } = this.employee;
         this.first_name = first_name;
         this.last_name = last_name;
-        this.password = <string>password;
+        this.username = username;
         this.admin = admin;
       }
+    },
+    clearUser() {
+      this.employee = undefined;
     }
   }
 });
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="stylus" scoped>
+#edit-user
+    width 80%
+</style>
