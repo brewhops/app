90b7cfd83d21636409c635575c67bc083e5923f2
danvanhorn <dan.vanhorn17@gmail.com>
2019-08-25 20:22:06 -0700

    chore: use new staging env variable

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,8 @@ Vue.config.productionTip = false;
 
 Vue.use(VueResource);
 
+process.env.VUE_APP_API = process.env.NODE_ENV === 'production-live' ? process.env.VUE_APP_API : process.env.VUE_APP_API_STAGING;
+
 new Vue({
   router,
   store,
