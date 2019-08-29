591ca98e4db86aca85beff0ec1b4ae14a37b09a8
Brennan Douglas <brennand97@gmail.com>
2019-08-28 22:03:36 -0700

    feat(auth): add auth token to all requests

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
+import Cookie from 'js-cookie';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,13 @@ import store from './store';
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
+
+Vue.http.interceptors.push((request: any) => {
+  const loggedIn = Cookie.getJSON('loggedIn');
+  if (loggedIn && !request.headers.get('Authorization')) {
+    request.headers.set('Authorization', `Bearer ${loggedIn.token}`);
+  }
+});
 
 new Vue({
   router,
