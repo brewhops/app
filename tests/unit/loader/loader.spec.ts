72a5e15dcc252b5c5b32271dcb0bcaf8d73429b8
danvanhorn <dan.vanhorn17@gmail.com>
2019-09-15 20:36:21 -0700

    feat: update jest test config

--- tests/unit/loader/loader.spec.ts
+++ tests/unit/loader/loader.spec.ts
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import Loader from '@/components/loader.vue'
 
 describe('loader.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('renders loader component', () => {
     const wrapper = shallowMount(Loader);
     expect(wrapper).toMatchSnapshot();
   })
